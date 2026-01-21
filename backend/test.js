eafccac6d2253b3ab44a8f3690482a3c15b194f0
lapsapthong-16 <honedwina@gmail.com>
2026-01-21 23:38:24 +0800

    update csv ingestion path

--- backend/test.js
+++ backend/test.js
@@ -4,17 +4,8 @@ async function runTests() {
     try {
         console.log("Starting Test...");
 
-        // Trying CSV - WORKS!
-        // const filePath = "C:/Users/Joon/Downloads/archive (4)/November Sleep Data - Sheet1.csv";
-
-        // Trying Excel - WORKS!
-        // const filePath = "C:/Users/Joon/Downloads/Coca Cola Co.xlsx";
-
-        // Trying PDF (TEXT) - WORKS!
-        // const filePath = "C:/Users/Joon/Downloads/trial.pdf"; // Text only
-        // const filePath = "C:/Users/Joon/Downloads/trial-image.pdf"; // Image only
-        // Mixed PDF - WORKS!
-        const filePath = "C:/Users/Joon/Downloads/Boukhayma_3D_Hand_Shape_and_Pose_From_Images_in_the_Wild_CVPR_2019_paper.pdf"; // Mixed
+        // Testing with Video Game Sales dataset
+        const filePath = "./backend/vgsales.csv";
 
         const result = await ingestFile(filePath);
 
